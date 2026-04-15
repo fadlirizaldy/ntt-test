02c995d792d27a41a15aa8ea4b5fad3deb6b4456
Fadli Rizaldy <fadly.rizaldy@ai.astra.co.id>
2026-04-15 18:44:42 +0700

    init project

---
+++ src/routes/index.tsx
@@ -0,0 +1,32 @@
+// src/routes/index.tsx
+import { createBrowserRouter } from "react-router";
+import DashboardLayout from "../layouts/DashboardLayout";
+import Login from "../pages/Login";
+import Home from "../pages/Home";
+import Product from "../pages/Product";
+import ProductDetail from "../pages/ProductDetail";
+
+export const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/",
+    element: <DashboardLayout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "product",
+        element: <Product />,
+      },
+      {
+        path: "product/:id",
+        element: <ProductDetail />,
+      },
+    ],
+  },
+]);
